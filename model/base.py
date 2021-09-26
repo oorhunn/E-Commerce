64a1875ed0f5b6de57c10cf03f06b430f6d8a909
anil_ <anil_demirgolu@hotmail.com>
2021-09-26 16:10:03 +0300

    todo update

--- model/base.py
+++ model/base.py
@@ -2,7 +2,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-engine = create_engine('postgresql://postgres:mt98mxb9r2@localhost:5432/duruh')
+engine = create_engine('postgresql://postgres:aaaaa@localhost:5432/duruh')
 Session = sessionmaker(bind=engine)
 
 Base = declarative_base()
